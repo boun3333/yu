2a06615cc7d9176bde8dafa24d2a4ade3294b017
boun3333 <86645256+boun3333@users.noreply.github.com>
2023-03-16 17:31:46 +0900

    Update deploy.sh
    efwef

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+t=`ps -efa | grep springboots2idemo | grep -v grep | awk '{print $1}'`
+if [ -z $t ];then
+   echo "Tomcat  was not started."
+else
+ p=`ps -efa | grep springboots2idemo | grep -v grep | awk '{print $2}'`
+ echo $p
+ kill -9 $p
+ echo "tomcat killllllll"
+fi
 cd /home/ec2-user/test/deploy
 nohup java -jar springboots2idemo-0.1.1-SNAPSHOT.jar > /home/ec2-user/test/deploy/nohup.out 2>&1 &
