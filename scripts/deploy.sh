bc52e5382f279ebefd28df486c2e55164cef33e4
boun3333 <86645256+boun3333@users.noreply.github.com>
2023-03-16 16:03:08 +0900

    Update deploy.sh
    fwefw

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
+t=`ps -efa | grep springboots2idemo | grep -v grep | awk '{print $1}'`
+if [ -z $t ];then
+   echo "Tomcat  was not started."
+else
+ p=`ps -efa | grep springboots2idemo | grep -v grep | awk '{print $2}'`
+ echo $p
+ kill -9 $p
+ echo "tomcat killllllll"
+fi
 
 REPOSITORY=/home/ec2-user/test
 cd $REPOSITORY
