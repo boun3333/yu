0959016e984a53a8c43277eaa87bb9bc28946cdd
boun3333 <86645256+boun3333@users.noreply.github.com>
2023-03-16 17:27:35 +0900

    Update deploy.sh
    ewfwef

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
-sh /home/ec2-user/abc.sh
-sleep 5
-sh /home/ec2-user/test.sh
+#!/bin/bash
+cd /home/ec2-user/test/deploy
+nohup java -jar springboots2idemo-0.1.1-SNAPSHOT.jar > /home/ec2-user/test/deploy/nohup.out 2>&1 &
